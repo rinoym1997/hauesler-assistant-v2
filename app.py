e5076e51d31ef1ed6c760d12deff3dbe7f9b2526
rinoym1997 <rinoymtrash@gmail.com>
2026-06-30 19:26:33 +0200

    Fix: langchain_text_splitters import + requirements

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ Seminararbeit "Artifacts in IT" | MSc Wirtschaftsinformatik | Gruppe ARTI
 import os
 import streamlit as st
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import Chroma
 from langchain.chains import ConversationalRetrievalChain
